9a0a48c0f0388f843068ac3e91df3922eda9bfe7
William <williampv@gmail.com>
2025-07-19 10:51:33 -0500

    EmployeeDbContext class created

---
+++ Model/EmployeeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Employee.API.Model
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) 
+        {
+        }
+
+    }
+}
