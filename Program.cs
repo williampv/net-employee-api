1b7c1628425b4cfc4acf06315f006b915daf35ce
William <williampv@gmail.com>
2025-07-19 10:56:50 -0500

    DbContext class registration

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Employee.API.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDatabase")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
